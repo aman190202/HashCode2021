3766d71a1919016cd952f576815fc0d9880817fa
aman190202 <agarwalaman190202@gmail.com>
2021-01-08 00:06:51 +0530

    Update ReadFile.py
    Added a sorting method to put all the unique ingredients in the list

--- ReadFile.py
+++ ReadFile.py
@@ -1,9 +1,23 @@
+
 if __name__=='__main__':
-  a=open("//content//a_example") #Reads the file
+  #Creating an extension to read a file and extract its elements
+  a=open("C:\\Users\\agarw\\OneDrive\\Desktop\\HashCode2021\\a_example") #Reads the file
   b=a.readline()                 #Reads the first line of the file
   b=b.split()                    #Splits the contents of the file
-  l=list()                       
+  l=list()                      
   for x in range(int(b[0])):    
     c=a.readline()      
     c=c.split()
     l.append(c)
+  #Picking out the unique ingredients used in all the pizzas in the variabe "ing"
+  ing=list()
+  for x in l:
+  	for y in x:
+  		if(y.isnumeric()):
+  			continue
+  		if(y in ing):
+  			continue
+  		else:
+  			ing.append(y)
+  print(ing)
+
