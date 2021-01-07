d91a78a00670e53ece55fa9fab924ffdb6dd3bb3
Aman Agarwal <48821634+aman190202@users.noreply.github.com>
2021-01-07 21:52:03 +0530

    Add files via upload

---
+++ ReadFile.py
@@ -0,0 +1,9 @@
+if __name__=='__main__':
+  a=open("//content//a_example") #Reads the file
+  b=a.readline()                 #Reads the first line of the file
+  b=b.split()                    #Splits the contents of the file
+  l=list()                       
+  for x in range(int(b[0])):    
+    c=a.readline()      
+    c=c.split()
+    l.append(c)
