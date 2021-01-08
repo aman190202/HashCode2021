89224e2ebf4160b69855438a7f70600fcbdc2ab9
aman190202 <agarwalaman190202@gmail.com>
2021-01-08 19:13:29 +0530

    Update ReadFile.py
    Ranking system made

--- ReadFile.py
+++ ReadFile.py
@@ -28,14 +28,41 @@ def twotothree(l,ing):
       if y in ing:
         x.remove(y)
     x.remove(x[2])
-  return l    
+  return l     
+def ranking(l):
+  a=list()
+  for x in range(len(l)):
+    a.append(l[x][2])
+  return a
+def ranking1(e,l):
+  o=list()
+  for x in e:
+    z=list()
+    for y in e:
+      a=len(set(x)&set(y))
+      z.append(a)
+    o.append(z)
+  for x in range(len(l)):
+    g=l[x][1]/2
+    for x1 in range(len(o[x])):
+      if(o[x][x1]>=g):
+        o[x][x1]=0
+      else:
+        o[x][x1]=1
+  return o
+def feeder(o,l):
+  i=0
+  for x in o:
+    l[i].append(x)
+    i+=1
+  return l
+
 if __name__=='__main__':
   
   a=open("C:\\Users\\agarw\\OneDrive\\Desktop\\HashCode2021\\a_example") #Reads the file
   b=a.readline()                                     
   b=b.split()                                       
-  l=list()
-  T=[[2],[3],[4]]                     
+  l=list()                     
   for x in range(int(b[0])):                                
     c=a.readline()      
     c=c.split()
@@ -46,6 +73,10 @@ if __name__=='__main__':
   l=convStrtoInt(l)      #Converting the string part of the list into integer
   l=addNumbering(l)        #Added numbering to pizza list for easy naviagtion of a pizza
   #l- [pizza number,number of pizza ingredients,pizza ingredients.......]
-  l.sort(reverse=True,key=len)
   l=twotothree(l,ing)
+  e=ranking(l)
+  o=ranking1(e,l)
+  l=feeder(o,l)
+  
+  
   
