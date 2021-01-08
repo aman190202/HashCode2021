55a1a2c00058eafce81c337ab7b7236797f5c20a
aman190202 <agarwalaman190202@gmail.com>
2021-01-08 11:29:39 +0530

    Update ReadFile.py
    Made a function to pick out all the unique elements

--- ReadFile.py
+++ ReadFile.py
@@ -1,15 +1,5 @@
-
-if __name__=='__main__':
-  #Creating an extension to read a file and extract its elements
-  a=open("C:\\Users\\agarw\\OneDrive\\Desktop\\HashCode2021\\a_example") #Reads the file
-  b=a.readline()                 #Reads the first line of the file
-  b=b.split()                    #Splits the contents of the file
-  l=list()                      
-  for x in range(int(b[0])):    
-    c=a.readline()      
-    c=c.split()
-    l.append(c)
-  #Picking out the unique ingredients used in all the pizzas in the variabe "ing"
+#Function to pick out all the ingredients
+def uniqueIngredients(l):
   ing=list()
   for x in l:
   	for y in x:
@@ -19,5 +9,21 @@ if __name__=='__main__':
   			continue
   		else:
   			ing.append(y)
-  print(ing)
+  return ing
 
+
+if __name__=='__main__':
+  
+  #Creating an extension to read a file and extract its elements
+  a=open("C:\\Users\\agarw\\OneDrive\\Desktop\\HashCode2021\\a_example") #Reads the file
+  b=a.readline()                 #Reads the first line of the file
+  b=b.split()                    #Splits the contents of the file b[1]-number of 2; b[2]- number of 3 ; b[3]-number of 4
+  l=list()
+  T=[[2],[3],[4]]                     
+  for x in range(int(b[0])):    
+    c=a.readline()      
+    c=c.split()
+    l.append(c)
+  
+  #Picking out the unique ingredients used in all the pizzas in the variabe "ing"
+  ing=uniqueIngredients(l)
