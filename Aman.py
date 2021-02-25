63189f0158f3b7f7e7d008e83f1fdc48d6aa8cf4
aman190202 <agarwalaman190202@gmail.com>
2021-02-26 00:04:45 +0530

    Update Aman.py

--- Aman.py
+++ Aman.py
@@ -8,9 +8,14 @@ if __name__=='__main__':
   S=int(b[2])
   V=int(b[3])
   F=int(b[4])
-  l=list()
+  l1=list()
   for x in range(S):                                
     c=a.readline()      
     c=c.split()
-    l.append(c)
-  print(l)
+    l1.append(c)
+  l2=list()
+  for x in range(V):
+    e=a.readline()
+    e=e.split()
+    l2.append(e)
+  print(l2)
