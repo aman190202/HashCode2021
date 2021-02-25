c72e8475c7e7d2f83c6fdd51a82abde0071bdaf5
joelsunny12 <joelsunny041002@gmail.com>
2021-02-26 00:17:21 +0530

    Update JoelSunny.py

--- JoelSunny.py
+++ JoelSunny.py
@@ -8,14 +8,21 @@ if __name__=='__main__':
   S=int(b[2])
   V=int(b[3])
   F=int(b[4])
+
+
   l1=list()
+  l2=list()
   for x in range(S):                                
     c=a.readline()      
     c=c.split()
+    c[0]=int(c[0])
+    c[1]=int(c[1])
+    c[3]=int(c[3])
     l1.append(c)
   l2=list()
   for x in range(V):
     e=a.readline()
     e=e.split()
+    e[0]=int(e[0])
     l2.append(e)
-  print(l2)
+  print(l1)
