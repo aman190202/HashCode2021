fcb689deb9eccdae991c6a1b366132c620490dc4
aman190202 <agarwalaman190202@gmail.com>
2021-02-25 23:49:22 +0530

    Update Aman.py

---
+++ Aman.py
@@ -0,0 +1,17 @@
+if __name__=='__main__':
+  
+  a=open("C:\\Users\\agarw\\OneDrive\\Desktop\\a.txt") #Reads the file
+  b=a.readline()                                     
+  b=b.split()
+  D=int(b[0])
+  I=int(b[1])
+  S=int(b[2])
+  V=int(b[3])
+  F=int(b[4])
+
+
+  l=list()                     
+  for x in range(S):                                
+    c=a.readline()      
+    c=c.split()
+    l.append(c)
